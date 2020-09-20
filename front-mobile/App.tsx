f0ab8d72003876223bfd9074cb6abf88e4dfa3f5
Evelyn Rocha <evelyn.tecnologia@outlook.com>
2020-09-20 19:54:37 -0300

    Adicionando rotas no React native

--- front-mobile/App.tsx
+++ front-mobile/App.tsx
@@ -1,22 +1,34 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import Header from "./src/components/Header";
-import Home from "./src/pages/Home";
+import { AppLoading } from "expo";
+import {
+  useFonts,
+  Play_400Regular,
+  Play_700Bold,
+} from "@expo-google-fonts/play";
+import Routes from "./src/routes";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Header />
-      <Home />
-      <StatusBar style="auto" />
-    </View>
-  );
+  let [fontsLoaded] = useFonts({
+    Play_400Regular,
+    Play_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <View style={styles.container}>
+        <Routes />
+        <StatusBar style="light" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#0B1F34",
   },
 });
