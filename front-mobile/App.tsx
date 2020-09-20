b1dfa415d3f6c1552993f064ba8a4d141d48c9f3
Evelyn Rocha <evelyn.tecnologia@outlook.com>
2020-09-20 19:23:07 -0300

    Adicionando pagina home no React native

---
+++ front-mobile/App.tsx
@@ -0,0 +1,22 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Header from "./src/components/Header";
+import Home from "./src/pages/Home";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Header />
+      <Home />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#0B1F34",
+  },
+});
